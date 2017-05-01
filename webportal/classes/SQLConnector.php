034f09985a81080f9ed4d4a29eb2dd5843d15dd8
David Garry <dgarry@usc.edu>
2017-04-30 18:13:41 -0700

    color changes

--- webportal/classes/SQLConnector.php
+++ webportal/classes/SQLConnector.php
@@ -110,7 +110,7 @@ class SQLConnector {
   */
   public function getConversations() {
     $conversations = "";
-    for ($x = 0; $x <= 50; $x++) {
+    for ($x = 0; $x <= 150; $x++) {
       $convname = "Conversation " . $x . " Test";
       $fullconvname = $convname;
       // Truncate conversation name to 28 characters max
@@ -128,11 +128,12 @@ class SQLConnector {
   */
   public function getMessages() {
     $messages = "";
-    for ($x = 0; $x <= 15; $x++) {
+    for ($x = 0; $x <= 50; $x++) {
       if($x & 1) {
-        $messages .= "<div class='message-link animated wow flipInX bubble'>" . "Lorem ipsum dolor sit amet, consectetur adipiscing elit " . $x . ".</div><br>";
+        //can add for profile pictures: <img class='profile-picture' src='img/no-text.png'></img>
+        $messages .= "<div class='message-link bubble'>" . "<span class='author-link'>David:</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit " . $x . ".</div><br>";
       } else {
-        $messages .= "<div class='message-link animated wow flipInX bubble bubble--alt'>" . "Ut enim ad minim veniam, quis nostrud " . $x . ".</div><br>";
+        $messages .= "<div class='message-link bubble bubble--alt'>" . "Ut enim ad minim veniam, quis nostrud " . $x . ".</div><br>";
       }
     }
     return $messages;
